9935aa36113e2c492a909fdf8372983ae604eb20
nazaifcru <nazaif@seeroo.com>
2020-04-07 14:21:07 +0530

    Drawer & FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primarySwatch: Colors.green
-  ),
-  home: MyHomePage(),
-  debugShowCheckedModeBanner: false,
-));
-
-
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -17,35 +13,47 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Calling Bell App"),
       ),
-      body: Center(
-        child: Container(
-          width: 120,
-          height: 120,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(15),
-            gradient: LinearGradient(
-              colors: [Colors.purple, Colors.yellow]
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Moideen Nazaif VM"),
+              accountEmail: Text("nazaifmoid@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://secure.meetupstatic.com/photos/member/8/5/b/4/highres_269014228.jpeg"),
+              ),
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10
-              )
-            ]
-          ),
-          child: Text("I'm Awesome developer",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          alignment: Alignment.center,
-        )
-      ),
 
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Nazaif Moideen",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              subtitle: Text("iOS Developer"),
+              trailing: Icon(Icons.edit),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Nazaif Moideen",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              subtitle: Text("Flutter Developer"),
+              trailing: Icon(Icons.edit),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.near_me),
+      ),
     );
   }
 }
