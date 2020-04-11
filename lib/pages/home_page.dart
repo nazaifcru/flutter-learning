dd9b3a8647c7cdfa4f07abd18404a7ef88a4d3ea
nazaifcru <nazaif@seeroo.com>
2020-04-11 16:57:24 +0530

    Networking

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:callingbell/name_card_widget.dart';
 import 'package:callingbell/drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -9,8 +10,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var _myText = "Change my Future";
-  TextEditingController _controller = TextEditingController();
+  var url = "https://api.github.com/users";
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    print(res.body);
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +36,26 @@ class _MyHomePageState extends State<MyHomePage> {
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _myText = _controller.text;
-          setState(() {});
+
         },
         child: Icon(Icons.near_me),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: NameCardWidget(myText: _myText, controller: _controller),
-          ),
-        ),
-      ),
+      body: data != null ? ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(data[index]["login"]),
+              subtitle: Text("ID:${data[index]["id"]}"),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  data[index]["avatar_url"]
+                ),
+              )
+            );
+          },
+      itemCount: data.length,)
+          : Center(
+        child: CircularProgressIndicator(),
+      )
     );
   }
 }
